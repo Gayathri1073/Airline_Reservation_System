f8966768e609b2eb6380399e0d89d505edf83405
Gayathri1073 <134733240+Gayathri1073@users.noreply.github.com>
2024-08-25 02:47:06 +0530

    Add files via upload

---
+++ AirlineReservationSystem.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+#include "learnprogramo.cpp"
+using namespace std;
+
+int main()
+{
+
+	system("CLS");
+	
+	cout << "\t    AIRLINE RESERVATION SYSTEM \n";
+
+
+	menu();
+	seat_fill();
+
+	return 0;
+
+
+}
+
+
+
